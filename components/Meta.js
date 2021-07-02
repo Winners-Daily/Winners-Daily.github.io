552ee20ce2ba7b25306aa289ff8b24f23e623d39
Amir Anwar Azzam <amir.azzam@students.salle.url.edu>
2021-07-02 11:18:17 +0200

    top logo fix

--- components/Meta.js
+++ components/Meta.js
@@ -7,7 +7,7 @@ const Meta = ({ title, keywords, description }) => {
       <meta name='keywords' content={keywords} />
       <meta name='description' content={description} />
       <meta charSet='utf-8' />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='/WD.png' />
       <title>{title}</title>
     </Head>
   )
