b03b24d128009d8a861b9d28471b4a9852cc616f
Amir Anwar Azzam <amir.azzam@students.salle.url.edu>
2021-07-05 16:06:27 +0200

    ANALYTICS

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,26 @@ import Footer from './Footer'
 import Meta from './Meta'
 import styles from '../styles/Layout.module.css'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../lib/ga'
+
 const Layout = ({ children }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+      const handleRouteChange = (url) => {
+        ga.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+  }, [router.events])
+
   return (
     <>
       <Meta />
